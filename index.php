b8454f0bf1d713214c70c3861b4a49e39f91288a
Jaagup Aland <jaagala@users.noreply.github.com>
2018-09-11 14:24:39 +0300

    Lisatud esimene fail

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Minu esimene sait ;)</title>
+	</head>
+	<body style="background-color:#e8f6ff;">
+		<h1>Jaagup Aland</h1>
+		<p>Siin on minu <a href="https://www.tlu.ee/">TLÜ</a> õppetöö</p>
+		<h6>See veebileht on õppetöö eesmärgil tehtud.</h6>
+		
+		<a href="https://upload.wikimedia.org/wikipedia/commons/4/41/Edgar_Savisaar_2005.jpg"><img src="../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt="pildike"> </a>
+		<p> Minu semu teeb ka <a href="../../~priilau" target="_blank">veebi</a> :P</p>
+
+	</body>
+	
+</html>
